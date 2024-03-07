b29de701602b14ea8d14070cdc2def585807789d
Artem <arzamachev.art-alex@yandex.ru>
2024-03-07 19:59:14 +0300

    add unit for backend

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -9,6 +9,35 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
+    
+    def test_mul(self):
+        self.assertEqual(self.calculator.multiplication(5, 6), 30)
+
+    def test_sub(self):
+        self.assertEqual(self.calculator.subtraction(20, 27), -7)
+
+    def test_div(self):
+        self.assertEqual(self.calculator.division(30, 6), 5)
+        self.assertEqual(self.calculator.division(30, 0), None)
+
+    def test_abs(self):
+        self.assertEqual(self.calculator.absolute(4), 4)
+        self.assertEqual(self.calculator.absolute(-7), 7)
+
+    def test_degree(self):
+        self.assertEqual(self.calculator.degree(2, 10), 1024)
+
+    def test_ln(self):
+        self.assertEqual(self.calculator.ln(1), 0)
+
+    def test_log(self):
+        self.assertEqual(self.calculator.log(64, 4), 3)
+
+    def test_sqrt(self):
+        self.assertEqual(self.calculator.sqrt(625), 25)
+
+    def test_nth_sqrt(self):
+        self.assertEqual(self.calculator.nth_root(0, 36), 0)
 
 
 if __name__ == "__main__":
