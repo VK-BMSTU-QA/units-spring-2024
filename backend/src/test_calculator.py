92b8c2095f20cea83edd7994865cd70975ec5f81
khristina <hristinakirillova2559@gmail.com>
2024-03-07 19:17:30 +0300

    add test to back

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -9,6 +9,52 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
+        self.assertEqual(self.calculator.addition(0, 0), 0)
+        self.assertEqual(self.calculator.addition(0, 90), 90)
+        self.assertEqual(self.calculator.addition(34789, -30), 34759)
+        self.assertEqual(self.calculator.addition(77447, 677889), 755336)
+
+    def test_multiplicat(self):
+        self.assertEqual(self.calculator.multiplication(1, 2), 2)
+        self.assertEqual(self.calculator.multiplication(0, 0), 0)
+        self.assertEqual(self.calculator.multiplication(0, 1), 0)
+        self.assertEqual(self.calculator.multiplication(34789, -30), -1043670)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtraction(1, 2), 2)
+        self.assertEqual(self.calculator.subtraction(0, 0), 0)
+        self.assertEqual(self.calculator.subtraction(0, 1), 0)
+        self.assertEqual(self.calculator.subtraction(34789, -30), -1043670)
+
+    def test_divis(self):
+        self.assertEqual(self.calculator.division(1, 2), 2)
+        self.assertEqual(self.calculator.division(0, 0), 0)
+        self.assertEqual(self.calculator.division(0, 1), 0)
+        self.assertEqual(self.calculator.division(34789, -30), -1043670)
+
+    def test_deg(self):
+        self.assertEqual(self.calculator.degree(1, 2), 2)
+        self.assertEqual(self.calculator.degree(0, 0), 0)
+        self.assertEqual(self.calculator.degree(0, 1), 0)
+        self.assertEqual(self.calculator.degree(34789, -30), -1043670)
+
+    def test_ln(self):
+        self.assertEqual(self.calculator.ln(1, 2), 2)
+        self.assertEqual(self.calculator.ln(0, 0), 0)
+        self.assertEqual(self.calculator.ln(0, 1), 0)
+        self.assertEqual(self.calculator.ln(34789, -30), -1043670)
+
+    def test_sqrt(self):
+        self.assertEqual(self.calculator.sqrt(1, 2), 2)
+        self.assertEqual(self.calculator.sqrt(0, 0), 0)
+        self.assertEqual(self.calculator.sqrt(0, 1), 0)
+        self.assertEqual(self.calculator.sqrt(34789, -30), -1043670)
+
+    def nth_root(self):
+        self.assertEqual(self.calculator.nth_root(1, 2), 2)
+        self.assertEqual(self.calculator.nth_root(0, 0), 0)
+        self.assertEqual(self.calculator.nth_root(0, 1), 0)
+        self.assertEqual(self.calculator.nth_root(34789, -30), -1043670)
 
 
 if __name__ == "__main__":
