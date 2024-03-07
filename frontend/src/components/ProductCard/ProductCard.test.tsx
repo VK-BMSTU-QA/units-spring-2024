d9e6ea68fe4f942697cee65a09fbbb35e03097fb
Maxim Sikanov <max.4251474@yandex.ru>
2024-03-07 20:50:33 +0300

    test ProductCard.tsx

---
+++ frontend/src/components/ProductCard/ProductCard.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ProductCard } from './ProductCard';
+import {Category, PriceSymbol} from "../../types";
+
+const mockProduct = {
+    id: 1,
+    name: 'Test Product',
+    description: 'This is a test product',
+    price: 100,
+    priceSymbol: '$' as PriceSymbol,
+    category: 'Одежда' as Category,
+    imgUrl: 'test_image.jpg',
+};
+
+afterEach(jest.clearAllMocks);
+
+describe('ProductCard test', () => {
+    it('should render correctly', () => {
+        const { asFragment } = render(<ProductCard {...mockProduct} />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should display product details correctly', () => {
+        const { getByText, getByAltText } = render(
+            <ProductCard {...mockProduct} />
+        );
+
+        expect(getByText('Test Product')).toBeInTheDocument();
+        expect(getByText('This is a test product')).toBeInTheDocument();
+        expect(getByText('100 $')).toBeInTheDocument();
+        expect(getByText('Одежда')).toBeInTheDocument();
+        expect(getByAltText('Test Product')).toBeInTheDocument();
+    });
+
+    it('should not render image if imgUrl is not provided', () => {
+        const { queryByAltText } = render(
+            <ProductCard {...mockProduct} imgUrl={undefined} />
+        );
+
+        expect(queryByAltText('Test Product')).not.toBeInTheDocument();
+    });
+});
