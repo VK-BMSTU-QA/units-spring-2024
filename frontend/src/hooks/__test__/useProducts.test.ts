5d01a206d998d8ebf006d75db1400d0d0bc35dbc
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-03-08 20:55:15 +0300

    Added useProducts hook tests

---
+++ frontend/src/hooks/__test__/useProducts.test.ts
@@ -0,0 +1,16 @@
+import { useProducts } from '../useProducts';
+import { renderHook } from '@testing-library/react';
+
+describe('test useProducts hook', () => {
+    it('should return products list', () => {
+        const { result } = renderHook(() => useProducts());
+        expect(result.current.length).toBeGreaterThan(0);
+    });
+
+    it('should return the same product list every time', () => {
+        const { result, rerender } = renderHook(() => useProducts());
+        const prev = result.current;
+        rerender();
+        expect(result.current).toEqual(prev);
+    });
+});
