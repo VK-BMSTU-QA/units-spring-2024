cc995a8fe9ea270c9a9dfce7b4dbeafa4cf9ee71
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2024-03-09 16:33:21 +0300

    add check fot getPrice call in ProductCard test

--- frontend/src/components/ProductCard/ProductCard.test.tsx
+++ frontend/src/components/ProductCard/ProductCard.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { ProductCard } from "./ProductCard";
+import { ProductCard } from './ProductCard';
 import { Product } from '../../types/Product.js';
+import { PriceSymbol } from '../../types/Symbol';
+import * as utils from '../../utils';
+
+jest.mock('../../utils', () => ({
+    getPrice: jest.fn((value: number, symbol: PriceSymbol) => '1000 ₽'),
+}));
 
 afterEach(jest.clearAllMocks);
 describe('ProductCards test', () => {
@@ -19,5 +25,6 @@ describe('ProductCards test', () => {
         const rendered = render(<ProductCard {...testProduct} />);
 
         expect(rendered.asFragment()).toMatchSnapshot();
+        expect(utils.getPrice).toHaveBeenCalledTimes(1);
     });
 });
