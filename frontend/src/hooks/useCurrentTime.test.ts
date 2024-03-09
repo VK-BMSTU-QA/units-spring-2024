555a28058699ef01f12e8c6ebcf622c718f07211
alfaiv <alfaiv@yandex.ru>
2024-03-09 08:03:46 +0000

    fix front

--- frontend/src/hooks/useCurrentTime.test.ts
+++ frontend/src/hooks/useCurrentTime.test.ts
@@ -17,12 +17,12 @@ describe('use Current Time tests', () => {
   it('returns the current time', () => {
     const {result} = renderHook(useCurrentTime);
 
-    expect(result.current).toBe('0:00:00');
+    expect(result.current).toBe('00:00:00');
 
     act(() => {
         jest.advanceTimersByTime(1000);
       });
 
-    expect(result.current).toBe('0:00:01');
+    expect(result.current).toBe('00:00:01');
   });
 });
