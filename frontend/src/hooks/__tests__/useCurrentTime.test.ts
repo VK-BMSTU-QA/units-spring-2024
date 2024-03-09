b1814f1d7f3cf65adc39ed6529b86969e428fd8e
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2024-03-09 17:05:29 +0300

    update useCurrentTime test

--- frontend/src/hooks/__tests__/useCurrentTime.test.ts
+++ frontend/src/hooks/__tests__/useCurrentTime.test.ts
@@ -6,18 +6,23 @@ afterEach(jest.restoreAllMocks);
 
 describe('test use current time function', () => {
     it('should update current time every second', () => {
+        const currDate = new Date('2024-09-03T12:00:00.000Z');
+        jest.spyOn(global, 'Date').mockImplementation(() => currDate);
+
         const updateCurrTimeHook = renderHook(() => useCurrentTime());
 
         const initialTime = updateCurrTimeHook.result.current;
-        expect(initialTime).toBe(new Date().toLocaleTimeString('ru-RU'));
+        expect(initialTime).toBe(currDate.toLocaleTimeString('ru-RU'));
 
         act(() => {
             jest.advanceTimersByTime(1000);
         });
 
+        const updatedDate = new Date('2024-09-03T12:00:01.000Z');
+        jest.spyOn(global, 'Date').mockImplementation(() => updatedDate);
+
         const updatedTime = updateCurrTimeHook.result.current;
-        expect(updatedTime).toBe(new Date().toLocaleTimeString('ru-RU'));
-        expect(updatedTime).not.toBe(initialTime);
+        expect(updatedTime).toBe(updatedDate.toLocaleTimeString('ru-RU'));
     });
 
     it('should clear the interval when component is unmounted', () => {
@@ -28,4 +33,8 @@ describe('test use current time function', () => {
 
         expect(clearIntervalMock).toHaveBeenCalled();
     });
+});
+
+afterAll(() => {
+    jest.useRealTimers();
 });
