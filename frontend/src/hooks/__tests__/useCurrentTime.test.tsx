bc5184d9f85993e5b468acf248477fda39b84404
Stoma Fedor <fedorstoma2@gmail.com>
2024-03-17 20:26:59 +0300

    fix jest time

--- frontend/src/hooks/__tests__/useCurrentTime.test.tsx
+++ frontend/src/hooks/__tests__/useCurrentTime.test.tsx
@@ -5,7 +5,9 @@ describe('test useCurrentTime function', () => {
     jest.useFakeTimers();
 
     it('should return the current time', () => {
-        const expected = new Date().toLocaleTimeString('ru-RU');
+        jest.setSystemTime(new Date(2024, 3, 17, 0, 0, 0));
+
+        const expected = '00:00:00';
         
         const { result } = renderHook(() => useCurrentTime());
         expect(result.current).toBe(expected);
