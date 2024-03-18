f0f6578c8474006ea80d33b34f2abd491414ee79
Alexander Tarba <sachatarba@rambler.ru>
2024-03-19 00:02:49 +0300

    Добавить в фиксацию обновления тестов на бек

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -8,157 +8,270 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
 
-    def test_add1(self):
-        self.assertEqual(self.calculator.addition(1, 2), 1 + 2)
+    # тесты функции add
+    def test_add_int(self):
+        self.assertEqual(self.calculator.addition(1, 2), 3)
 
-    def test_add2(self):
-        self.assertEqual(self.calculator.addition(2, 1), 2 + 1)
+    def test_add_none(self):
+        self.assertRaises(TypeError, self.calculator.addition, None, 1)
 
-    def test_add3(self):
-        self.assertEqual(self.calculator.addition(-1, 2), -1 + 2)
+    def test_add_none_str(self):
+        self.assertRaises(TypeError, self.calculator.addition, None, "1")
 
-    def test_add4(self):
-        self.assertEqual(self.calculator.addition(2, -1), 2 + -1)
+    def test_add_int_comm(self):
+        self.assertEqual(self.calculator.addition(2, 1), 3)
 
-    def test_add5(self):
-        self.assertEqual(self.calculator.addition(2, 0), 2 + 0)
+    def test_add_neg(self):
+        self.assertEqual(self.calculator.addition(-1, 2), 1)
 
-    def test_add6(self):
-        self.assertAlmostEqual(self.calculator.addition(2.3, 3), 2.3 + 3)
+    def test_add_neg_comm(self):
+        self.assertEqual(self.calculator.addition(2, -1), 1)
 
-    def test_add7(self):
-        self.assertAlmostEqual(self.calculator.addition(2.3, 3.1), 2.3 + 3.1)
+    def test_add_zero(self):
+        self.assertEqual(self.calculator.addition(2, 0), 2)
 
-    def test_mul1(self):
-        self.assertEqual(self.calculator.multiplication(3, 2), 3 * 2)
+    def test_add_one_float(self):
+        self.assertAlmostEqual(self.calculator.addition(2.3, 3), 5.3)
 
-    def test_mul2(self):
-        self.assertEqual(self.calculator.multiplication(2, 3), 2 * 3)
+    def test_add_two_float(self):
+        self.assertAlmostEqual(self.calculator.addition(2.3, 3.1), 5.4)
 
-    def test_mul3(self):
-        self.assertEqual(self.calculator.multiplication(10, 0), 10 * 0)
+    # тесты функции mul
+    def test_mul_int(self):
+        self.assertEqual(self.calculator.multiplication(3, 2), 6)
 
-    def test_mul4(self):
-        self.assertAlmostEqual(self.calculator.multiplication(10.1, 2), 10.1 * 2)
+    def test_mul_none(self):
+        self.assertRaises(TypeError, self.calculator.multiplication, None, 1)
 
-    def test_mul5(self):
-        self.assertAlmostEqual(self.calculator.multiplication(10.1, 2.1), 10.1 * 2.1)
+    def test_mul_lst_str(self):
+        self.assertRaises(TypeError, self.calculator.multiplication, [], "")
 
-    def test_mul6(self):
-        self.assertEqual(self.calculator.multiplication(-10, 2), -10 * 2)
+    def test_mul_lst_none(self):
+        self.assertRaises(TypeError, self.calculator.multiplication, [], None)
 
-    def test_mul7(self):
-        self.assertAlmostEqual(self.calculator.multiplication(-10.2, 2), -10.2 * 2)
+    def test_mul_int_comm(self):
+        self.assertEqual(self.calculator.multiplication(2, 3), 6)
 
-    def test_mul8(self):
-        self.assertAlmostEqual(self.calculator.multiplication(-10.2, 2.1), -10.2 * 2.1)
+    def test_mul_zero(self):
+        self.assertEqual(self.calculator.multiplication(10, 0), 0)
 
-    def test_mul9(self):
-        self.assertAlmostEqual(self.calculator.multiplication(-10.2, -2.1), -10.2 * -2.1)
+    def test_mul_one_float(self):
+        self.assertAlmostEqual(self.calculator.multiplication(10.1, 2), 20.2)
 
-    def test_sub1(self):
-        self.assertEqual(self.calculator.subtraction(3, 2), 3 - 2)
+    def test_mul_two_float(self):
+        result = 10.1 * 2.1
+        self.assertAlmostEqual(self.calculator.multiplication(10.1, 2.1), result)
 
-    def test_div1(self):
-        self.assertAlmostEqual(self.calculator.division(3, 2), 3 / 2)
+    def test_mul_neg(self):
+        result = -10 * 2
+        self.assertEqual(self.calculator.multiplication(-10, 2), result)
 
-    def test_div2(self):
-        self.assertAlmostEqual(self.calculator.division(3.2, 2), 3.2 / 2)
+    def test_mul_one_float_neg(self):
+        result = -10.2 * 2
+        self.assertAlmostEqual(self.calculator.multiplication(-10.2, 2), result)
+
+    def test_mul_two_float_neg(self):
+        result = -10.2 * 2.1
+        self.assertAlmostEqual(self.calculator.multiplication(-10.2, 2.1), result)
+
+    def test_mul_two_float_two_neg(self):
+        result = -10.2 * -2.1
+        self.assertAlmostEqual(self.calculator.multiplication(-10.2, -2.1), result)
+
+    # тесты функции sub
+    def test_sub(self):
+        self.assertEqual(self.calculator.subtraction(3, 2), 1)
+
+    def test_sub_none(self):
+        self.assertRaises(TypeError, self.calculator.subtraction, None, 1)
+
+    def test_sub_lst_none(self):
+        self.assertRaises(TypeError, self.calculator.subtraction, [], 1)
+
+    def test_sub_neg(self):
+        self.assertEqual(self.calculator.subtraction(3, -2), 5)
+
+    def test_sub_two_neg(self):
+        self.assertEqual(self.calculator.subtraction(-3, -2), -1)
+
+    def test_sub_res_neg(self):
+        self.assertEqual(self.calculator.subtraction(1, -2), 3)
+
+    def test_sub_res_zero(self):
+        self.assertEqual(self.calculator.subtraction(2, 2), 0)
+
+    def test_sub_two_floats(self):
+        result = 3.1 - 2.1
+        self.assertEqual(self.calculator.subtraction(3.1, 2.1), result)
+
+    # тесты функции div
+    def test_div(self):
+        result = 3 / 2
+        self.assertAlmostEqual(self.calculator.division(3, 2), result)
+
+    def test_div_none(self):
+        self.assertRaises(TypeError, self.calculator.division, 1, None)
+
+    def test_div_lst_none(self):
+        self.assertRaises(TypeError, self.calculator.division, [], None)
+
+    def test_div_period(self):
+        result = 1 / 3
+        self.assertAlmostEqual(self.calculator.division(1, 3), result)
+
+    def test_div_one_float(self):
+        result = 3.2 / 2
+        self.assertAlmostEqual(self.calculator.division(3.2, 2), result)
     
-    def test_div3(self):
-        self.assertAlmostEqual(self.calculator.division(-3, 2), -3 / 2)
+    def test_div_one_neg(self):
+        result = -3 / 2
+        self.assertAlmostEqual(self.calculator.division(-3, 2), result)
 
-    def test_div4(self):
-        self.assertAlmostEqual(self.calculator.division(-3.2, 2), -3.2 / 2)
+    def test_div_one_neg_one_float(self):
+        result = -3.2 / 2
+        self.assertAlmostEqual(self.calculator.division(-3.2, 2), result)
 
-    def test_div5(self):
-        self.assertAlmostEqual(self.calculator.division(-3.2, -2), -3.2 / -2)
+    def test_div_two_neg_one_float(self):
+        result = -3.2 / -2
+        self.assertAlmostEqual(self.calculator.division(-3.2, -2), result)
 
-    def test_div6(self):
-        self.assertAlmostEqual(self.calculator.division(-3.2, -2.2), -3.2 / -2.2)
+    def test_div_two_neg_one_float(self):
+        result =  -3.2 / -2.2
+        self.assertAlmostEqual(self.calculator.division(-3.2, -2.2), result)
 
-    def test_div7(self):
+    def test_div_zero(self):
         self.assertEqual(self.calculator.division(10, 0), None)
     
-    def test_abs1(self):
+    # тесты на функцию abs
+    def test_abs(self):
         self.assertEqual(self.calculator.adsolute(3), 3)
 
-    def test_abs2(self):
+    def test_abs_none(self):
+        self.assertRaises(TypeError, self.calculator.adsolute, None)
+    
+    def test_abs_lst(self):
+        self.assertRaises(TypeError, self.calculator.adsolute, [])
+
+    def test_abs_neg(self):
         self.assertEqual(self.calculator.adsolute(-3), 3)
 
-    def test_abs3(self):
+    def test_abs_zero(self):
         self.assertEqual(self.calculator.adsolute(0), 0)
 
-    def test_abs4(self):
-        self.assertEqual(self.calculator.adsolute(3), 3)
-        
+    # тесты на функцию deg
     def test_deg(self):
-        self.assertEqual(self.calculator.degree(3, 2), 3 ** 2)
+        result = 3 ** 2
+        self.assertEqual(self.calculator.degree(3, 2), result)
 
-    def test_deg1(self):
-        self.assertAlmostEqual(self.calculator.degree(3, -2), 3 ** -2)
+    def test_deg_none(self):
+        self.assertRaises(TypeError, self.calculator.degree, 1, None)
+    
+    def test_deg_lst_none(self):
+        self.assertRaises(TypeError, self.calculator.degree,[], None)
+    
+    def test_deg_neg_deegre(self):
+        result = 3 ** -2
+        self.assertAlmostEqual(self.calculator.degree(3, -2), result)
 
-    def test_deg2(self):
-        self.assertEqual(self.calculator.degree(-3, 2), (-3) ** 2)
+    def test_deg_neg_num(self):
+        result = (-3) ** 2
+        self.assertEqual(self.calculator.degree(-3, 2), result)
 
-    def test_deg3(self):
-        self.assertAlmostEqual(self.calculator.degree(-3.1, -2), (-3.1) ** (-2))
+    def test_deg_float_negs(self):
+        result = (-3.1) ** (-2)
+        self.assertAlmostEqual(self.calculator.degree(-3.1, -2), result)
         
+    # тесты на функцию ln
     def test_ln(self):
-        self.assertAlmostEqual(self.calculator.ln(2), math.log(2))
+        result = math.log(2)
+        self.assertAlmostEqual(self.calculator.ln(2), result)
 
-    def test_ln1(self):
-        self.assertAlmostEqual(self.calculator.ln(2.1), math.log(2.1))
+    def test_ln_none(self):
+        self.assertRaises(TypeError, self.calculator.ln, None)
+    
+    def test_ln_lst(self):
+        self.assertRaises(TypeError, self.calculator.ln, [])
 
-    def test_log1(self):
-        self.assertAlmostEqual(self.calculator.log(2, 1.1), math.log(2, 1.1))
+    def test_ln_float(self):
+        result = math.log(2.1)
+        self.assertAlmostEqual(self.calculator.ln(2.1), result)
 
-    def test_log2(self):
-        self.assertAlmostEqual(self.calculator.log(2, 2), math.log(2, 2))
+    # тесты на функцию log
+    def test_log_float(self):
+        result =  math.log(2, 1.1)
+        self.assertAlmostEqual(self.calculator.log(2, 1.1), result)
 
-    def test_log3(self):
-        self.assertAlmostEqual(self.calculator.log(2, 1.5), math.log(2, 1.5))
+    def test_ln_none_lst(self):
+        self.assertRaises(TypeError, self.calculator.log, None, [])
 
-    def test_log4(self):
+    def test_log(self):
+        result =  math.log(2, 2)
+        self.assertAlmostEqual(self.calculator.log(2, 2), result)
+
+    def test_log_period(self):
+        result =  math.log(2, 1.5)
+        self.assertAlmostEqual(self.calculator.log(2, 1.5), result)
+
+    def test_log_neg_deg(self):
         with self.assertRaises(ValueError): 
             self.calculator.log(1, -5)
 
-    def test_log5(self):
+    def test_log_neg_num(self):
         with self.assertRaises(ValueError): 
             self.calculator.log(-1, 5)
 
-    def test_log6(self):
+    def test_log_two_neg(self):
         with self.assertRaises(ValueError): 
             self.calculator.log(-1, -5)
 
+    # тесты на функцию sqrt
     def test_sqrt(self):
-        self.assertAlmostEqual(self.calculator.sqrt(2), 2 ** 0.5)
-    
-    def test_sqrt1(self):
-        self.assertAlmostEqual(self.calculator.sqrt(2.1), 2.1 ** 0.5)
+        r = 2 ** 0.5
+        self.assertAlmostEqual(self.calculator.sqrt(2), r)
 
-    def test_sqrt2(self):
-        self.assertAlmostEqual(self.calculator.sqrt(-2), (-2) ** 0.5)
+    def test_log_none(self):
+        self.assertRaises(TypeError, self.calculator.sqrt, None)
+
+    def test_log_lst(self):
+        self.assertRaises(TypeError, self.calculator.sqrt, [])
     
+    def test_sqrt_float(self):
+        r = 2.1 ** 0.5
+        self.assertAlmostEqual(self.calculator.sqrt(2.1), r)
+
+    def test_sqrt_neg(self):
+        r = (-2) ** 0.5
+        self.assertAlmostEqual(self.calculator.sqrt(-2), r)
+    
+    # тесты на функцию nth_root
     def test_nth_root(self):
-        self.assertAlmostEqual(self.calculator.nth_root(2, 1), 2 ** (1. / 1))
-
-    def test_nth_root1(self):
-        self.assertAlmostEqual(self.calculator.nth_root(-2, 1), (-2) ** (1. / 1))
-
-    def test_nth_root2(self):
-        self.assertAlmostEqual(self.calculator.nth_root(-2, -1), (-2) ** (1. / -1))
-
-    def test_nth_root3(self):
-        self.assertAlmostEqual(self.calculator.nth_root(2, 3.1), 2 ** (1. / 3.1))
+        r = 2 ** (1. / 1)
+        self.assertAlmostEqual(self.calculator.nth_root(2, 1), r)
     
-    def test_nth_root4(self):
-        self.assertAlmostEqual(self.calculator.nth_root(2.1, 3.1), 2.1 ** (1. / 3.1))
+    def test_nth_root_lst_none(self):
+        self.assertRaises(TypeError, self.calculator.nth_root, None, [])
 
-    def test_nth_root5(self):
-        self.assertRaises(ValueError, self.calculator.log, 1, -5)
+    def test_nth_root_none_lst(self):
+        self.assertRaises(TypeError, self.calculator.nth_root, [], None)
+
+    def test_nth_root_neg(self):
+        r = (-2) ** (1. / 1)
+        self.assertAlmostEqual(self.calculator.nth_root(-2, 1), r)
+
+    def test_nth_root_two_neg(self):
+        r = (-2) ** (1. / -1)
+        self.assertAlmostEqual(self.calculator.nth_root(-2, -1), r)
+
+    def test_nth_root_float(self):
+        r = 2 ** (1. / 3.1)
+        self.assertAlmostEqual(self.calculator.nth_root(2, 3.1), r)
+    
+    def test_nth_root_two_float(self):
+        r = 2.1 ** (1. / 3.1)
+        self.assertAlmostEqual(self.calculator.nth_root(2.1, 3.1), r)
         
 
 
 if __name__ == "__main__":
     unittest.main()
+
