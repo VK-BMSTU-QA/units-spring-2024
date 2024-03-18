c0bb86527504ae3cfed8b70a72e719e589a503e3
Glibusss <expansifff@gmail.com>
2024-03-18 03:11:32 +0300

    some tests

--- frontend/src/hooks/__tests__/useCurrentTime.test.tsx
+++ frontend/src/hooks/__tests__/useCurrentTime.test.tsx
@@ -6,12 +6,17 @@ describe('test use current time hook', () => {
     test('should return the current time', () => {
         const { result } = renderHook(() => useCurrentTime());
 
-        expect(result.current).toBe(new Date().toLocaleTimeString('ru-RU'));
+        const testDate = new Date()
+        const hours = String(testDate.getHours()).length == 2 ? testDate.getHours():`0${testDate.getHours()}`;
+        const minutes = String(testDate.getMinutes()).length == 2 ? testDate.getMinutes():`0${testDate.getMinutes()}`;
+        const seconds = String(testDate.getSeconds()).length == 2 ? testDate.getSeconds():`0${testDate.getSeconds()}`;
+
+        expect(result.current).toBe(`${hours}:${minutes}:${seconds}`);
 
         act(() => {
             jest.advanceTimersByTime(10);
         });
 
-        expect(result.current).toBe(new Date().toLocaleTimeString('ru-RU'));
+        expect(result.current).toBe(`${hours}:${minutes}:${seconds}`);
     });
 });
