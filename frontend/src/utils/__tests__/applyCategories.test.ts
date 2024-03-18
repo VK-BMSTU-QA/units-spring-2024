79ca3e31a4b3f5f9c73acce933649072bd58d807
alfaiv <alfaiv@yandex.ru>
2024-03-18 22:51:39 +0300

    fix front

--- frontend/src/utils/__tests__/applyCategories.test.ts
+++ frontend/src/utils/__tests__/applyCategories.test.ts
@@ -1,34 +1,38 @@
 import { applyCategories } from '../applyCategories';
 import type { Product, Category } from '../../types';
 
+const products:Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description 1',
+      price: 10,
+      category: 'Электроника'
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Description 2',
+      price: 20,
+      category: 'Для дома'
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        description: 'Description 3',
+        price: 30,
+        category: 'Одежда'
+    },
+];
 
 describe('test apply Categories function', () => {
     it('should return products with current category', () => {
         
-        const products:Product[] = [
-            {
-              id: 1,
-              name: 'Product 1',
-              description: 'Description 1',
-              price: 10,
-              category: 'Электроника'
-            },
-            {
-              id: 2,
-              name: 'Product 2',
-              description: 'Description 2',
-              price: 20,
-              category: 'Для дома'
-            },
-            {
-                id: 3,
-                name: 'Product 3',
-                description: 'Description 3',
-                price: 30,
-                category: 'Одежда'
-            },
-        ];
+        const NULLcategory: Category[] = [];
+
         
+        expect(applyCategories(products, NULLcategory)).toEqual(products);
+
         const category: Category[] = ['Одежда'];
 
         
