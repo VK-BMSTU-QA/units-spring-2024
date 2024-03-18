3aa590ce34fa7dd6a239700fe8cbb94a07e5a712
Dan <kdanil01@mail.ru>
2024-03-18 20:29:08 +0300

    Type tests

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -1,6 +1,7 @@
 import unittest
 import math
-from src.calculator import Calculator
+#from src.calculator import Calculator
+from calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestCalculator(unittest.TestCase):
 
     def setUp(self):
         self.calculator = Calculator()
-
+        
     def test_add(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
 
@@ -33,6 +34,34 @@ class TestCalculator(unittest.TestCase):
 
     def test_add_negative_fraction(self):
         self.assertAlmostEqual(self.calculator.addition(-0.1, 1.1), 1, places=7)
+
+    def test_add_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition(None, 0)
+
+    def test_add_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition(0, "1")
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition("1", 0)
+
+    def test_add_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition(0, [1])
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition([1], 0)
+
+    def test_add_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition(0, {1: 1})
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.addition({1: 1}, 0)
 
     # Subtraction
 
@@ -59,6 +88,34 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtraction_negative_fraction_from_fraction(self):
         self.assertAlmostEqual(self.calculator.subtraction(3.2, -0.2), 3.4, places=7)
+
+    def test_subtraction_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction(None, 0)
+
+    def test_subtraction_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction(0, "1")
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction("1", 0)
+
+    def test_subtraction_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction(0, [1])
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction([1], 0)
+
+    def test_subtraction_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction(0, {1: 1})
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.subtraction({1: 1}, 0)
 
     # Multiplication
 
@@ -91,6 +148,28 @@ class TestCalculator(unittest.TestCase):
 
     def test_multiplication_fraction_result_is_negative_fraction(self):
         self.assertAlmostEqual(self.calculator.multiplication(2, -0.7), -1.4, places=7)
+    
+    def test_multiplication_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.multiplication(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.multiplication(None, 0)
+
+    def test_multiplication_string(self):
+        self.assertEqual(self.calculator.multiplication(2, "1"), "11")
+        self.assertEqual(self.calculator.multiplication("1", 2), "11")
+
+    def test_multiplication_list(self):
+        self.assertEqual(self.calculator.multiplication(2, [1]), [1, 1])
+        self.assertEqual(self.calculator.multiplication([1], 2), [1, 1])
+
+    def test_multiplication_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.multiplication(0, {1: 1})
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.multiplication({1: 1}, 0)
 
     # Division
         
@@ -130,6 +209,34 @@ class TestCalculator(unittest.TestCase):
     def test_division_by_fraction_negative_divisible(self):
         self.assertAlmostEqual(self.calculator.division(-5, 2.5), -2, places=7)
 
+    def test_division_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division(None, 1)
+
+    def test_division_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division(0, "1")
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division("1", 1)
+
+    def test_division_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division(0, [1])
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division([1], 1)
+
+    def test_division_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division(0, {1: 1})
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.division({1: 1}, 1)
+
     # Absolute
 
     def test_adsolute(self):
@@ -143,6 +250,25 @@ class TestCalculator(unittest.TestCase):
 
     def test_adsolute_fraction(self):
         self.assertAlmostEqual(self.calculator.adsolute(-3.2), 3.2)
+
+    def test_adsolute_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.adsolute(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.adsolute(None, 1)
+
+    def test_adsolute_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.adsolute("1")
+
+    def test_adsolute_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.adsolute([1])
+
+    def test_adsolute_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.adsolute({1: 1})
 
     # Degree ("power of" правильно)
 
@@ -170,6 +296,34 @@ class TestCalculator(unittest.TestCase):
     def test_degree_negative(self):
         self.assertAlmostEqual(self.calculator.degree(-2, 2), 4)
 
+    def test_degree_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree(None, 1)
+
+    def test_degree_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree(0, "1")
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree("1", 1)
+
+    def test_degree_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree(0, [1])
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree([1], 1)
+
+    def test_degree_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree(0, {1: 1})
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.degree({1: 1}, 1)
+
     # Ln
 
     def test_ln(self):
@@ -188,6 +342,25 @@ class TestCalculator(unittest.TestCase):
     def test_ln_zero(self):
         with self.assertRaises(Exception) as cm:
             self.calculator.ln(0)
+
+    def test_ln_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.ln(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.ln(None, 1)
+
+    def test_ln_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.ln("1")
+
+    def test_ln_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.ln([1])
+
+    def test_ln_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.ln({1: 1})
 
     # Log
 
@@ -223,6 +396,25 @@ class TestCalculator(unittest.TestCase):
         with self.assertRaises(Exception) as cm:
             self.calculator.log(2, 1)
 
+    def test_log_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.log(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.log(None, 1)
+
+    def test_log_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.log("1")
+
+    def test_log_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.log([1])
+
+    def test_log_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.log({1: 1})
+
     # Sqrt
 
     def test_sqrt(self):
@@ -237,6 +429,25 @@ class TestCalculator(unittest.TestCase):
     def test_sqrt_fraction(self):
         self.assertAlmostEqual(self.calculator.sqrt(0.25), 0.5)
 
+    def test_sqrt_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.sqrt(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.sqrt(None, 1)
+
+    def test_sqrt_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.sqrt("1")
+
+    def test_sqrt_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.sqrt([1])
+
+    def test_sqrt_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.sqrt({1: 1})
+
     # Nth root
 
     def test_nth_root(self):
@@ -250,6 +461,34 @@ class TestCalculator(unittest.TestCase):
 
     def test_nth_root_fractiom(self):
         self.assertAlmostEqual(self.calculator.nth_root(0.25, 2), 0.5)
+
+    def test_nth_root_none(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root(0, None)
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root(None, 1)
+
+    def test_nth_root_string(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root(0, "1")
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root("1", 1)
+
+    def test_nth_root_list(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root(0, [1])
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root([1], 1)
+
+    def test_nth_root_dict(self):
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root(0, {1: 1})
+            
+        with self.assertRaises(Exception) as cm:
+            self.calculator.nth_root({1: 1}, 1)
 
 
 if __name__ == "__main__":
