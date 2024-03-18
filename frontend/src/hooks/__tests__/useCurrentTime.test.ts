adc26ce301a6e742ed8449ec507dcf246cba29d5
Алексей Красноперов <alexkrasnoperov09@gmail.com>
2024-03-18 15:13:45 +0300

    front tests fixed

--- frontend/src/hooks/__tests__/useCurrentTime.test.ts
+++ frontend/src/hooks/__tests__/useCurrentTime.test.ts
@@ -3,23 +3,23 @@ import { renderHook, act } from '@testing-library/react';
 
 describe('test useCurrentTime', () => {
     it('returns the current time every second', () => {
-        jest.useFakeTimers();
+        jest.useFakeTimers().setSystemTime(new Date('2024-03-18 00:00:00'));
 
         const { result } = renderHook(() => useCurrentTime());
 
-        expect(result.current).toBe(new Date().toLocaleTimeString('ru-RU'));
+        expect(result.current).toBe('0:00:00');
 
         act(() => {
             jest.advanceTimersByTime(1000);
         });
 
-        expect(result.current).toBe(new Date().toLocaleTimeString('ru-RU'));
+        expect(result.current).toBe('0:00:01');
 
         act(() => {
             jest.advanceTimersByTime(2000);
         });
 
-        expect(result.current).toBe(new Date().toLocaleTimeString('ru-RU'));
+        expect(result.current).toBe('0:00:03');
 
         jest.useRealTimers();
     });
