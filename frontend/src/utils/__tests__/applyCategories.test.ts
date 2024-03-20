52e8b25b1ac1129ffff1800490388fb98a2ea4ad
Artem <arzamachev.art-alex@yandex.ru>
2024-03-20 19:43:59 +0300

    update tests

--- frontend/src/utils/__tests__/applyCategories.test.ts
+++ frontend/src/utils/__tests__/applyCategories.test.ts
@@ -4,9 +4,11 @@ import { useProducts } from '../../hooks';
 const products = useProducts();
 
 describe('test apply categories function', () => {
-    it('should return value needs products', () => {
+    it('should return an empty list of products with empty parameters', () => {
         expect(applyCategories([], [])).toStrictEqual([]);
+    });
 
+    it('should return all products with an empty list categories', () => {
         expect(applyCategories([
             {
                 id: 1,
@@ -52,36 +54,38 @@ describe('test apply categories function', () => {
         ]);
     });
 
-    expect(applyCategories([
-        {
-            id: 1,
-            name: '',
-            description: '',
-            price: 1000,
-            priceSymbol: '₽',
-            imgUrl: '',
-            category: 'Для дома',
-
-        },
-        {
-            id: 2,
-            name: '',
-            description: '',
-            price: 999999,
-            priceSymbol: '₽',
-            imgUrl: '',
-            category: 'Электроника',
-
-        },
-    ], ['Одежда', 'Для дома'])).toStrictEqual([
-        {
-            id: 1,
-            name: '',
-            description: '',
-            price: 1000,
-            priceSymbol: '₽',
-            imgUrl: '',
-            category: 'Для дома',
-        },
-    ]);
+    it('should return all products whose categories are contained in the transmitted list', () => {
+        expect(applyCategories([
+            {
+                id: 1,
+                name: '',
+                description: '',
+                price: 1000,
+                priceSymbol: '₽',
+                imgUrl: '',
+                category: 'Для дома',
+    
+            },
+            {
+                id: 2,
+                name: '',
+                description: '',
+                price: 999999,
+                priceSymbol: '₽',
+                imgUrl: '',
+                category: 'Электроника',
+    
+            },
+        ], ['Одежда', 'Для дома'])).toStrictEqual([
+            {
+                id: 1,
+                name: '',
+                description: '',
+                price: 1000,
+                priceSymbol: '₽',
+                imgUrl: '',
+                category: 'Для дома',
+            },
+        ]);
+    });
 });
--- frontend/src/utils/__tests__/updateCategories.test.ts
+++ frontend/src/utils/__tests__/updateCategories.test.ts
@@ -1,9 +1,15 @@
 import { updateCategories } from '../updateCategories';
 
 describe('test update categories function', () => {
-    it('should return value needs categories', () => {
+    it('should return all categories added to an empty list', () => {
         expect(updateCategories([], 'Для дома')).toStrictEqual(['Для дома']);
+    });
+
+    it('should return a list of categories by deleting the necessary ones', () => {
         expect(updateCategories(['Для дома'], 'Для дома')).toStrictEqual([]);
+    });
+    
+    it('should return a list of categories by adding new categories', () => {
         expect(updateCategories(['Для дома', 'Электроника'], 'Одежда')).toStrictEqual(['Для дома', 'Электроника', 'Одежда'])
     });
 });
