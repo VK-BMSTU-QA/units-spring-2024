555c3088014a950b75e9225ec228085cbf5fd00d
MrDjeb <MrDjeb245@gmail.com>
2024-03-20 15:57:47 +0300

    fix useCurrentTime

--- frontend/src/hooks/useCurrentTime.test.tsx
+++ frontend/src/hooks/useCurrentTime.test.tsx
@@ -1,8 +1,9 @@
 import { useCurrentTime } from './useCurrentTime';
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 
 describe('useCurrentTime', () => {
-  jest.useFakeTimers();
+  jest.useFakeTimers().setSystemTime(new Date('2024-01-01 00:00:00'));
+  
   it('returns the current time in true format', () => {
     const { result, unmount } = renderHook(() => useCurrentTime());
     expect(result.current).toMatch(/\d{2}:\d{2}:\d{2}/);
@@ -10,10 +11,13 @@ describe('useCurrentTime', () => {
     unmount();
   });
 
-  it('returns the current time in true locale', () => {
-    const { result, unmount } = renderHook(() => useCurrentTime());
-    expect(result.current).toBe(new Date().toLocaleTimeString('ru-RU'));
+  it('returns the current time', () => {
+    const { result } = renderHook(() => useCurrentTime());
 
-    unmount();
+    act(() => {
+      jest.advanceTimersByTime(1000 * 10);
+    });
+    expect(result.current).toBe('00:00:10');
   });
+
 });
